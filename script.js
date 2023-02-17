c5c08bf7a35613a0a9d83fd96c05f072209d910b
Kwiasek <86431269+Kwiasek@users.noreply.github.com>
2023-02-17 15:49:24 +0100

    Made UI, and simple functionality, but it needs to be fixed

--- script.js
+++ script.js
@@ -15,5 +15,62 @@ const divide = (a, b) => {
 }
 
 const operate = (operator, a, b) => {
-    operator == '+' ? add(a, b) : operator == '-' ? subtract(a, b) : operator == '*' ? multiply(a, b) : operator == '/' ? divide(a, b) : "Wrong operator";
+    if (operator == '+') return add(a, b);
+    if (operator == '-') return subtract(a, b);
+    if (operator == '*') return multiply(a, b);
+    if (operator == '/') return divide(a, b);
 }
+
+let displayValue = 0;
+let currentOperator = '';
+let firstNum = [];
+let secondNum = [];
+
+let buttons = document.querySelectorAll('.button');
+
+
+const changeDisplayNum = (displayValue) => {
+    document.querySelector('.display').innerHTML = displayValue;
+}
+
+changeDisplayNum(displayValue);
+
+const clear = () => {
+    displayValue = 0;
+    currentOperator = '';
+    firstNum = [];
+    secondNum = [];
+    changeDisplayNum(displayValue)
+}
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        if (!currentOperator && (Number(button.innerHTML) || button.innerHTML == '.')) {
+            if (firstNum.length < 11) {
+                firstNum.push(button.innerHTML);
+                displayValue = parseFloat(firstNum.join(''));
+                changeDisplayNum(displayValue);
+            }
+        }
+        if (button.innerHTML == '+' || button.innerHTML == '-' || button.innerHTML == '*' || button.innerHTML == '/' ) {
+            currentOperator = button.innerHTML;
+        }
+        if (currentOperator && (Number(button.innerHTML) || button.innerHTML == '.')) {
+            secondNum.push(button.innerHTML);
+            displayValue = parseFloat(secondNum.join(''));
+            changeDisplayNum(displayValue);
+        }
+        if (button.innerHTML == '=') {
+            if(firstNum && currentOperator && secondNum) {
+                let a = parseFloat(firstNum.join(''));
+                let b = parseFloat(secondNum.join(''));
+                displayValue = operate(currentOperator, a, b);
+                changeDisplayNum(displayValue);
+            }
+        }
+        if (button.innerHTML == 'C') {
+            clear();
+        }
+    })
+})
+
